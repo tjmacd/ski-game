8576e7102a2c2e07cbce30c9230df172df87afc8
Timothy MacDougall <timothy.macdougall@uoit.net>
2016-11-26 17:38:43 -0500

    Draws basic shape on canvas

---
+++ app/src/main/java/com/example/thaddeus/csci4100proj/CanvasView.java
@@ -0,0 +1,39 @@
+package com.example.thaddeus.csci4100proj;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CanvasView extends View {
+    public static final int SPRITE_WIDTH = 200;
+    public static final int SPRITE_HEIGHT = 200;
+    private Bitmap playerSprite;
+
+    public CanvasView(Context ctx){
+        super(ctx);
+        init();
+    }
+
+    public CanvasView(Context ctx, AttributeSet attr) {
+        super(ctx, attr);
+        init();
+    }
+
+    private void init() {
+        playerSprite = BitmapFactory.decodeResource(getResources(), R.drawable.player);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawRGB(255,255,255);
+
+        RectF position = new RectF(200, 200, 400, 400);
+
+        canvas.drawBitmap(playerSprite, 200, 200, null);
+    }
+}
