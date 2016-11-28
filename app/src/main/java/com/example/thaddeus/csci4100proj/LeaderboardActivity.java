1a3163dff38450ecacada9177eb13729576ca631
Timothy MacDougall <timothy.macdougall@uoit.net>
2016-11-28 14:11:53 -0500

    Fixed bugs in LeaderBoard

--- app/src/main/java/com/example/thaddeus/csci4100proj/LeaderboardActivity.java
+++ app/src/main/java/com/example/thaddeus/csci4100proj/LeaderboardActivity.java
@@ -1,6 +1,8 @@
 package com.example.thaddeus.csci4100proj;
 
 import android.content.res.AssetManager;
+import android.content.res.XmlResourceParser;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -39,63 +41,77 @@ import static java.lang.Thread.sleep;
 
 
 public class LeaderboardActivity extends AppCompatActivity {
+    private String url = "http://toastermonkey.co.nf/highscores.xml";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
 
-        final TextView textBox = (TextView)findViewById(R.id.textView2);
+        DownloadXMLTask downloadXMLTask = new DownloadXMLTask();
+        downloadXMLTask.execute(url);
+    }
+
+    public void showScores(NodeList nodeList){
+        TextView textBox = (TextView) findViewById(R.id.textView2);
         textBox.setText("Players:\n");
-        final TextView textBox2 = (TextView)findViewById(R.id.textView3);
+        TextView textBox2 = (TextView) findViewById(R.id.textView3);
         textBox2.setText("Score:\n");
+        for (int i = 0; i < nodeList.getLength(); i++) {
 
-        Thread thread = new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                try {
-
-                    try {
-
-                        URL url = new URL("http://toastermonkey.co.nf/highscores.xml");
-                        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-                        DocumentBuilder db = dbf.newDocumentBuilder();
-                        Document doc = db.parse(new InputSource(url.openStream()));
-                        doc.getDocumentElement().normalize();
-
-                        NodeList nodeList = doc.getElementsByTagName("player");
-
-                        for (int i = 0; i < nodeList.getLength(); i++) {
-
-                            Node node = nodeList.item(i);
-
-                            Element fstElmnt = (Element) node;
-                            NodeList nameList = fstElmnt.getElementsByTagName("name");
-                            Element nameElement = (Element) nameList.item(0);
-                            nameList = nameElement.getChildNodes();
-                            textBox.append(((Node) nameList.item(0)).getNodeValue() + "\n");
-
-                            NodeList scoreList = fstElmnt.getElementsByTagName("score");
-                            Element scoreElement = (Element) scoreList.item(0);
-                            scoreList = scoreElement.getChildNodes();
-                            textBox2.append(((Node) scoreList.item(0)).getNodeValue() + "\n");
-
-                        }
-                    } catch (Exception e) {
-                        System.out.println("XML Pasing Excpetion = " + e);
-                    }
+            Node node = nodeList.item(i);
 
 
+            Element fstElmnt = (Element) node;
+            NodeList nameList = fstElmnt.getElementsByTagName("name");
+            Element nameElement = (Element) nameList.item(0);
+            nameList = nameElement.getChildNodes();
+            textBox.append(((Node) nameList.item(0)).getNodeValue() + "\n");
 
-                    //Your code goes here
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+
+            NodeList scoreList = fstElmnt.getElementsByTagName("score");
+            Element scoreElement = (Element) scoreList.item(0);
+            scoreList = scoreElement.getChildNodes();
+            textBox2.append(((Node) scoreList.item(0)).getNodeValue() + "\n");
+
+        }
+    }
+
+    class DownloadXMLTask extends AsyncTask<String, Void, NodeList> {
+        private Exception exception = null;
+        private NodeList nodeList = null;
+
+        @Override
+        public NodeList doInBackground(String... params) {
+
+            try {
+
+                URL url = new URL(params[0]);
+                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                DocumentBuilder db = dbf.newDocumentBuilder();
+                Document doc = db.parse(new InputSource(url.openStream()));
+                doc.getDocumentElement().normalize();
+
+                nodeList = doc.getElementsByTagName("player");
+
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        });
 
-        thread.start();
+            return nodeList;
+        }
+
+        @Override
+        protected void onPostExecute(NodeList result){
+            if(exception != null) {
+                exception.printStackTrace();
+                return;
+            }
+
+            showScores(result);
+        }
     }
 
     public void returnToMain(View view){
